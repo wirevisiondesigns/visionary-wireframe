15face2afceae928872671312cd6f6cd61541419
wirevisiondesigns <wirevisiondesignsllc@gmail.com>
2025-12-10 10:24:56 -0500

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,11 +1,11 @@
-import Header from "@/components/Header";
-import Hero from "@/components/Hero";
-import Services from "@/components/Services";
-import WhyChooseUs from "@/components/WhyChooseUs";
-import Process from "@/components/Process";
-import About from "@/components/About";
-import ContactForm from "@/components/ContactForm";
-import Footer from "@/components/Footer";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import Services from "../components/Services";
+import WhyChooseUs from "../components/WhyChooseUs";
+import Process from "../components/Process";
+import About from "../components/About";
+import ContactForm from "../components/ContactForm";
+import Footer from "../components/Footer";
 
 const Index = () => {
   return (
